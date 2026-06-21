09cec4d708fda1733fe5f4b4c1984949bca76b19
Achileons <ardaagovic@gmail.com>
2026-06-22 00:52:05 +0300

    Initial Commit

---
+++ KuyumStok/KuyumStokApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuyumStokApp.swift
+//  KuyumStok
+//
+//  Created by Arda Agovic on 22.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct KuyumStokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
